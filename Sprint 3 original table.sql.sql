c4cdd398b995f0a1922ce5f7c5b9d2d98119a2ba
Mazwi <61007747+Mazwi94@users.noreply.github.com>
2020-02-19 10:25:07 +0200

    Delete Sprint 3 original table.sql.sql

--- Sprint 3 original table.sql.sql
+++
@@ -1,7 +0,0 @@
-Chips	Price	Quantity	Cooldrink	Price	Quantity	Chocolates	Price	Quantity	Pies	Price	Quantity	Fruits	Price	Quantity	Cupcakes	Price	Quantity	Veggies	Price	Quantity
-Simba	15	25	Coke	20	80	kit kat	10	30	Pepper Steak	10	25	Apple	5	25	Vanilla	25	5	Spinach	10	5	124
-Lays	18	30	Fanta	20	30	Lunch bar	8.5	10	Steak and Kidney	8.5	30	Pear	5.5	30	Chocolate	20	30	Cabbage	10	30	1456
-Doretos	16.5	26	Pepsi	16.5	60	P.S	6	33	Sausage roll	6	60	Banana	3	60	Strawberry	33	2	Carrots	12	2	1463
-Cheese Curls	10	22	Jive	10	22	Rolo	8	50	Burger pie	8	10	Orange	4.5	10	Blueberry	35	10	Butternut	13.5	10	4568
-Jumping Jacks	12	22	Kingsley	12	39	Bar One	10.5	60	Cheese and Ham	8.5	50	Plum	10.5	33	Caramel	35	20	Tomatoes	16	23	1453
-Nik Naks	12	22	Refresh	10	50	Tex	8.5	60	Chicken	8.5	55	Litchi	8.5	50	Rasberry	20	30	Beetroot	20	30	4425
